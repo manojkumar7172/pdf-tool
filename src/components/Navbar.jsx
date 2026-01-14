b7695033c76922ab469d7dcb341e427900e51d5a
manojkumar7172 <manojkumar.achanta9@gmail.com>
2026-01-14 17:49:42 +0000

    added bg to navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,13 +2,13 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <div className='flex justify-between items-center'>
+    <div className='flex justify-between items-center  bg-blue-200'>
         <div className='flex items-center'>
            <h1 className='flex text-lg items-center justify-start'>Fuse PDF</h1> 
             <span className='text-base'>Merge PDFs securely in your browser </span>
         </div>
         <div>
-            
+
         </div>
     </div>
   )
